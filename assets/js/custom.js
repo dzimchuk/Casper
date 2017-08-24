18cea583fc15aa3e193ff6e3bea23c1a788e3c9d
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-08-24 13:51:46 +0300

    never hide social icons so there are no visible dom changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,6 @@
     "use strict";
 
     $(document).ready(function () {
-
         if (typeof github_url !== 'undefined') { $('.social-link-github').removeClass('hidden'); $('.social-link-github').attr('href', github_url); }
         if (typeof linkedin_url !== 'undefined') { $('.social-link-linkedin').removeClass('hidden'); $('.social-link-linkedin').attr('href', linkedin_url); }
     });
