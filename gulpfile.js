1f0a3127d7795c3d912a9153dda1178643a8f827
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-08-24 13:23:38 +0300

    use a single minimized theme script file

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var livereload = require('gulp-livereload');
 var nodemon = require('gulp-nodemon');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -24,7 +26,7 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
+gulp.task('build', ['css', 'scripts'], function (/* cb */) {
     return nodemonServerInit();
 });
 
@@ -45,8 +47,16 @@ gulp.task('css', function () {
         .pipe(livereload());
 });
 
+gulp.task('scripts', function() {
+    return gulp.src(['assets/js/*.js'])
+        .pipe(uglify())
+        .pipe(concat('theme.min.js'))
+        .pipe(gulp.dest('assets/built/'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
+    gulp.watch('assets/js/*.js', ['scripts']);
 });
 
 gulp.task('default', ['build'], function () {
